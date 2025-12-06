c5280ffcf3cd25b26cecaf6b51bc004de7d13f5f
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2025-12-06 03:22:54 -0600

    feat: search bar and filter

--- src/packetworld/controller/FXMLCollaboratorsController.java
+++ src/packetworld/controller/FXMLCollaboratorsController.java
@@ -10,6 +10,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,10 +19,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
@@ -39,6 +46,10 @@ public class FXMLCollaboratorsController implements Initializable {
 
     @FXML
     private TextField searchField;
+    @FXML
+    private Label lblFilter;
+    @FXML
+    private Label lblSearch;
     @FXML
     private TableColumn<Collaborator, String> colCurp;
     @FXML
@@ -67,6 +78,9 @@ public class FXMLCollaboratorsController implements Initializable {
     @FXML
     private Button btnAssign;
     private ObservableList<Collaborator> collaboratorsList;
+    private FilteredList<Collaborator> filteredData;
+
+    private String filterType = "General";
 
     /**
      * Initializes the controller class.
@@ -76,6 +90,7 @@ public class FXMLCollaboratorsController implements Initializable {
         configureTableColumns();
         loadData();
         configureTableSelection();
+        configureSearchFilter();
         tvCollaborators.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     }
 
@@ -98,8 +113,121 @@ public class FXMLCollaboratorsController implements Initializable {
 
         collaboratorsList.add(new Collaborator("2023001", "Marco", "Antonio", "Solis", "SAM900101HDFRRN010", "buki@gmail.com", "Administrador", "Sucursal Central", ""));
         collaboratorsList.add(new Collaborator("2023002", "Juan", "Gabriel", "Tejano", "TGJ950202MSDFRRN02", "juanga@hotmail.com.com", "Conductor", "Sucursal Norte", "20180102"));
+        collaboratorsList.add(new Collaborator("2023003", "Pepe", "Pica", "Papas", "PPP900101HDFRRN010", "ppp@gmail.com", "Ejecutivo de tienda", "Sucursal Sur", ""));
+        collaboratorsList.add(new Collaborator("2023004", "Valentin", "Elizalde", "Fermín", "VLE950202MSDFRRN02", "elizalde@hotmail.com.com", "Conductor", "Sucursal Central", "30180201"));
+        collaboratorsList.add(new Collaborator("2023005", "Elizabeth Angela", "Ferro", "Valentín", "SAM900101HDFRRN010", "eli@gmail.com", "Administrador", "Sucursal Sur", ""));
+        collaboratorsList.add(new Collaborator("2023006", "Gabriel", "Montiel", "Ferro", "WRV950202MSDFRRN02", "wero@hotmail.com.com", "Conductor", "Surcusal Central", "6080100"));
 
-        tvCollaborators.setItems(collaboratorsList);
+        filteredData = new FilteredList<>(collaboratorsList, p -> true);
+
+        SortedList<Collaborator> sortedData = new SortedList<>(filteredData);
+
+        sortedData.comparatorProperty().bind(tvCollaborators.comparatorProperty());
+
+        tvCollaborators.setItems(sortedData);
+    }
+
+    private void configureSearchFilter() {
+        searchField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(collaborator -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                String lowerCaseFilter = newValue.toLowerCase().trim();
+
+                if (lowerCaseFilter.isEmpty()) {
+                    return true;
+                }
+
+                String fullName = (collaborator.getName() + " " + collaborator.getLastname() + " " + collaborator.getSurname()).toLowerCase();
+                String personalNumber = collaborator.getPersonalNumber().toLowerCase();
+                String role = collaborator.getRole().toLowerCase();
+
+                switch (filterType) {
+                    case "Nombre(s)":
+                        return collaborator.getName().toLowerCase().contains(lowerCaseFilter);
+
+                    case "Apellido Paterno":
+                        return collaborator.getLastname().toLowerCase().contains(lowerCaseFilter);
+
+                    case "Apellido Materno":
+                        return collaborator.getSurname().toLowerCase().contains(lowerCaseFilter);
+
+                    case "No. Personal":
+                        return personalNumber.contains(lowerCaseFilter);
+
+                    case "Rol":
+                        return role.contains(lowerCaseFilter);
+
+                    case "General":
+                    default:
+                        if (fullName.contains(lowerCaseFilter)) {
+                            return true;
+                        }
+                        if (personalNumber.contains(lowerCaseFilter)) {
+                            return true;
+                        }
+                        if (role.contains(lowerCaseFilter)) {
+                            return true;
+                        }
+                        return false;
+                }
+            });
+        });
+    }
+
+    @FXML
+    private void handleFilterIconClick(MouseEvent event) {
+        ContextMenu contextMenu = new ContextMenu();
+
+        ToggleGroup group = new ToggleGroup();
+
+        RadioMenuItem itemGeneral = createFilterOption("General (Todos los campos)", group, true);
+        RadioMenuItem itemNombre = createFilterOption("Nombre(s)", group, false);
+        RadioMenuItem itemPaterno = createFilterOption("Apellido Paterno", group, false);
+        RadioMenuItem itemMaterno = createFilterOption("Apellido Materno", group, false);
+        RadioMenuItem itemPersonal = createFilterOption("Número de Personal", group, false);
+        RadioMenuItem itemRol = createFilterOption("Rol", group, false);
+
+        contextMenu.getItems().addAll(
+                itemGeneral,
+                itemNombre,
+                itemPaterno,
+                itemMaterno,
+                itemPersonal,
+                itemRol
+        );
+
+        contextMenu.show(lblFilter, event.getScreenX(), event.getScreenY());
+    }
+
+    private RadioMenuItem createFilterOption(String text, ToggleGroup group, boolean isSelected) {
+        RadioMenuItem item = new RadioMenuItem(text);
+        item.setToggleGroup(group);
+        item.setSelected(isSelected || text.startsWith(filterType));
+
+        item.setOnAction(e -> {
+            if (text.contains("General")) {
+                filterType = "General";
+            } else {
+                filterType = text;
+            }
+
+            String currentSearch = searchField.getText();
+            searchField.setText("");
+            searchField.setText(currentSearch);
+
+            searchField.setPromptText("Buscar por: " + filterType);
+        });
+
+        return item;
+    }
+
+    @FXML
+    private void handleDeleteSearch(MouseEvent event) {
+        searchField.setText("");
+        searchField.requestFocus();
     }
 
     private void configureTableSelection() {
@@ -147,13 +275,13 @@ public class FXMLCollaboratorsController implements Initializable {
         }
     }
 
-@FXML
+    @FXML
     private void handleDeleteCollaborator(ActionEvent event) {
         Collaborator selected = tvCollaborators.getSelectionModel().getSelectedItem();
-        
+
         if (selected != null) {
-            String mensaje = "¿Está seguro que desea eliminar al colaborador " 
-                    + selected.getPersonalNumber() + ": " 
+            String mensaje = "¿Está seguro que desea eliminar al colaborador "
+                    + selected.getPersonalNumber() + ": "
                     + selected.getName() + " " + selected.getLastname() + " " + selected.getSurname() + "?\n"
                     + "Recuerde que esta acción es irreversible.";
 
@@ -161,7 +289,7 @@ public class FXMLCollaboratorsController implements Initializable {
 
             if (confirmarEliminacion) {
                 collaboratorsList.remove(selected);
-                
+
                 Utility.createNotification("Colaborador eliminado exitosamente", NotificationType.DELETE);
             }
         }
